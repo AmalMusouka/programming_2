63249960f5482d6e0ce16d0c935ae8bac2024e67
AmalMusouka <amalsudhikumar20@gmail.com>
2025-03-26 14:13:39 +0100

    feat: initialized rectangles hw

---
+++ ant/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static long GetClosestPerfectRoot(long square)
+    {
+        double x = Double.Ceiling(Math.Sqrt(square));
+        double y = Double.Floor(Math.Sqrt(square));
+        if (Math.Abs((x * x) - square) >= Math.Abs((y * y) - square))
+        {
+            return (long)y;
+        }
+        return (long)x;
+    }
+    static (long, long) GetCoords(long input)
+    {
+        long row = 0;
+        long col = 0;
+        
+        long x = GetClosestPerfectRoot(input);
+        long diff = Math.Abs(input - (x * x));
+
+        if ((x * x) % 2 == 0)
+        {
+            if ((x * x) > input)
+            {
+                col = x - 1;
+                row = diff;
+            }
+            else if ((x * x) == input)
+            {
+                col = x - 1;
+                row = diff;
+            }
+            else
+            {
+                col = x;
+                row = Math.Abs(diff - 1);
+            }
+        }
+        else
+        {
+            if ((x * x) > input)
+            {
+                row = x - 1;
+                col = diff;
+            }
+            else if ((x * x) == input)
+            {
+                row = x - 1;
+                col = diff;
+            }
+            else
+            {
+                row = x;
+                col = Math.Abs(diff - 1);
+            }
+        }
+
+        return (row, col);
+    }
+
+
+    static void Main(string[] args)
+    {
+        while (Console.ReadLine() is string line)
+        {
+            long l = Int64.Parse(line.Replace("_", ""));
+            (long, long) coords = GetCoords(l);
+            Console.WriteLine($"{coords.Item1} {coords.Item2}");
+        }
+
+    }
+}
---
+++ rectangles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    class Rectangle
+    {
+        public (int x, int y) p;
+        public (int x, int y) q;
+
+        public Rectangle((int, int) p, (int, int) q)
+        {
+            this.p = p;
+            this.q = q;
+        }
+
+        public (int, int) upperLeft() => (p.x, p.y);
+
+        public (int, int) upperRight() => (q.x, p.y);
+
+        public (int, int) LowerLeft() => (p.x, q.y);
+        public (int, int) lowerRight() => (q.x, q.y);
+        public string str() => $"({p.x},{p.y}) - ({q.x},{q.y})";
+
+        public int perimeter() => 2 * (Math.Abs(p.y - q.y) + Math.Abs(p.x - q.x));
+        
+        public int area() => Math.Abs(p.x - q.x) * Math.Abs(p.y - q.y);
+
+        public bool contains(Rectangle r)
+        {
+            if (r.p.x < p.x) return false;
+            return true;
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        Rectangle r = new Rectangle((2, 3), (8, 10));
+        Rectangle s = new Rectangle((5, 5), (10, 7));
+        Rectangle t = new Rectangle((2, 8), (3, 10));
+        Rectangle u = new Rectangle((4, 6), (6, 8));
+
+        Console.WriteLine(r.upperLeft());        // (2, 3)
+        Console.WriteLine(r.lowerRight());       // (8, 10)
+        Console.WriteLine(r.str());              // (2, 3) - (8, 10)
+        Console.WriteLine(r.perimeter());
+        Console.WriteLine(r.area());
+    }
+}
