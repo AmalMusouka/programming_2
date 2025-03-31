6be9db515d675794d4873b326ef2ccd838dddff2
amalmusouka <amalsudhikumar20@gmail.com>
2025-03-31 11:57:32 +0200

    feat: initialized week 7 lec

---
+++ week6/Program.cs
@@ -0,0 +1,128 @@
+﻿using System.Numerics;
+
+//count how many integers in the array a satisfy a condition
+
+int count(int[] a, IntCondition cond)//Condition<int> cond
+{
+    int n = 0;
+    foreach (int i in a)
+    {
+        if (cond(i))
+        {
+            n += 1;
+        }
+    }
+
+    return n;
+}
+
+U[] map<T, U>(T[] a, Func<T, U> f)
+{
+    U[] b = new U[a.Length];
+    for (int i = 0; i < a.Length; ++i)
+    {
+        b[i] = f(a[i]);
+    }
+
+    return b;
+}
+
+
+bool is_odd(int i) => i % 2 == 1;
+
+//IntCondition c = is_odd;
+//Console.WriteLine(c(4));
+
+int[] a = [3, 4, 5, 6, 7];
+Interval interv = new(5, 100);
+Console.WriteLine(count(a, interv.contains));
+
+delegate bool IntCondition(int i);//Condition<T>
+
+class Interval
+{
+    private int low, high;
+
+    public Interval(int low, int high)
+    {
+        this.low = low;
+        this.high = high;
+    }
+
+    public bool contains(int i) => low <= i && i <= high;
+}
+
+
+//sum of numbers
+T sum<T>(T[] a) where T : INumber<T>
+{
+    T s = T.Zero;
+    foreach (T x in a)
+    {
+        s += x;
+    }
+
+    return s;
+}
+
+//write 'wc': word count
+
+try
+{
+    int words = 0;
+    foreach (string s in File.ReadLines(args[0]))
+    {
+        words += s.Split().Length;
+    }
+
+    Console.WriteLine($"{words:n0} words");
+}
+catch (IOException)
+{
+    Console.WriteLine("file not found");
+}
+
+
+// binary search tree class
+
+class Node<T>
+{
+    public T val;
+    public Node<T> left, right;
+
+    public Node(T val, Node<T>? left, Node<T>? right)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// a set of values of type T
+class TreeSet<T> where T: IComparable<T>
+{
+    private Node<T>? root = null;
+
+    bool contains(T x)
+    {
+        Node<T>? p = root;
+        while (p != null)
+        {
+            int i = x.CompareTo(p.val);
+            if (i < 0)
+            {
+                p = p.left;
+            }
+            else if (i > 0)
+            {
+                p = p.right;
+            }
+            else
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+} 
---
+++ week7/Program.cs
@@ -0,0 +1,99 @@
+﻿using Pos = (int x, int y);
+using static System.Math;
+
+int[] coins = [1, 2, 5, 10, 20];
+
+
+void print(string s) => Console.WriteLine(s);
+
+// Print all ways to form n using valid coins
+void change(int n)
+{
+    Stack<int> nums = [];
+
+    void go(int rem) // rem = remaining sum to make
+    {
+        if (rem == 0)
+        {
+            print(string.Join(" + ", nums));
+        }
+        else
+        {
+            foreach (int c in coins)
+            {
+                if (c <= rem)
+                {
+                    nums.Push(c);
+                    go(rem - c);
+                    nums.Pop();
+                }
+            }
+        }
+    }
+     // Print all ways to form a sum of (rem), using coins whose value is <= (max)
+    void maxGo(int rem, int max)
+    {
+        if (rem == 0)
+        {
+            print(string.Join(" + ", nums.Reverse()));
+        }
+        else
+        {
+            foreach (int c in coins)
+            {
+                if (c <= rem && c <= max)
+                {
+                    nums.Push(c);
+                    maxGo(rem - c, c);
+                    nums.Pop();
+                }
+            }
+        }
+    }
+
+    maxGo(n, n);
+}
+
+void draw_board(int n, Stack<Pos> queens)
+{
+    for (int y = 0; y < n; ++y)
+    {
+        for (int x = 0; x < n; ++x)
+        {
+         Console.Write(queens.Contains((x, y)) ? "Q " : ". ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+// Return true if queens at p and q attack each other.
+bool attack(Pos p, Pos q) => p.x == q.x || p.y == q.y || Abs(p.x - q.x) == Abs(p.y - q.y);
+
+void queens(int n)
+{
+    Stack<Pos> queens = [];
+
+    void place(int y)
+    {
+        if (y == n)
+        {
+            draw_board(n, queens);
+        }
+        else
+        {
+            for (int x = 0; x < n; ++x)
+            {
+                if (!queens.Any(q => attack(q, (x, y))))
+                {
+                    queens.Push((x, y));
+                    place(y + 1);
+                    queens.Pop();
+                }
+            }
+        }
+    }
+    place(0);
+}
+
+
+queens(int.Parse(args[0]));
