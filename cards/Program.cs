9dbb66dc70c28ee844b0a8df2ca466a6341c4bf0
AmalMusouka <amalsudhikumar20@gmail.com>
2025-04-05 20:48:50 +0200

    feat: finalized cards problem

--- cards/Program.cs
+++ cards/Program.cs
@@ -1,2 +1,111 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Card
+{
+    private Dictionary<string, int>converter = new Dictionary<string, int>
+    {
+        {"A", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5},
+        {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}, {"10", 10}, 
+        {"J", 11}, {"Q", 12}, {"K", 13}
+
+    };
+    // 1 = Ace, 2 .. 10, 11 = Jack, 12 = Queen, 13 = King 
+    public int rank;
+    public char suit;
+
+    public Card(string s)
+    {
+        string[] array = s.Select(c => c.ToString()).ToArray();
+        rank = array.Length > 2 ? converter[array[0] + array[1]] : converter[array[0]];
+        suit = char.Parse(array[^1]);
+    }
+}
+
+public class Hand
+{
+    public List<Card> cards;
+    public Hand(string hand)
+    {
+        cards = hand.Split(' ').Select(c => new Card(c)).ToList();
+        cards.Sort((a, b) =>
+        {
+            if (a.rank != 1 && b.rank != 1)
+            {
+                return b.rank - a.rank;
+            }
+            return a.rank - b.rank;
+        });
+    }
+    public (int, int) mostFrequent()
+    {
+        int highestCount = 0;
+        int count = 0;
+        int mostFrequentRank = cards[0].rank;
+        int currentRank = cards[0].rank;
+        foreach (Card c in cards)
+        {
+            if (currentRank == c.rank)
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+            }
+
+            if (count > highestCount)
+            {
+                highestCount = count;
+                mostFrequentRank = currentRank;
+            }
+            currentRank = c.rank;
+        }
+        return (highestCount, mostFrequentRank);
+    }
+    public void display()
+    {
+        foreach (Card c in cards)
+        {
+            Console.WriteLine("{0} {1}", c.rank, c.suit);
+        }
+    }
+
+    public int compare(Hand g)
+    {
+        (int, int) freq1 = mostFrequent();
+        (int, int) freq2 = g.mostFrequent();
+        if (freq1.Item1 > freq2.Item1)
+        {
+            return 1;
+        }
+        
+        if (freq1.Item1 < freq2.Item1)
+        {
+            return -1;
+        }
+        
+        if (freq1.Item2 > freq2.Item2)
+        {
+            if (freq2.Item2 == 1)
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+        
+        if (freq1.Item2 < freq2.Item2)
+        {
+            if (freq1.Item2 == 1)
+            {
+                return 1;
+            }
+            return -1;
+        }
+        
+        return 0;
+    }
+}
+
+
--- cards/obj/Debug/net9.0/cards.AssemblyInfo.cs
+++ cards/obj/Debug/net9.0/cards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e97885dcb706490c0739c33898d772d5357e331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bf5c647d27089fd7e462f1cff71c485852eaed")]
 [assembly: System.Reflection.AssemblyProductAttribute("cards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
