ff4b8c189b26136cb2b2afcc54d7e028450576d5
AmalMusouka <amalsudhikumar20@gmail.com>
2025-04-05 15:08:46 +0200

    feat: cards program initialized

--- cards/obj/Debug/net9.0/cards.AssemblyInfo.cs
+++ cards/obj/Debug/net9.0/cards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d38a10ab71a6e02cbe42f29716a04ff39b43bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e97885dcb706490c0739c33898d772d5357e331")]
 [assembly: System.Reflection.AssemblyProductAttribute("cards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
